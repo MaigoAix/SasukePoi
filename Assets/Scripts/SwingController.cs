f9b4b26ba645f1f05435dfd72a51a94ccb2324a6
MaigoAix <131898761+MaigoAix@users.noreply.github.com>
2023-05-24 18:32:33 +0100

    0.2 SwingPhysics

--- Assets/Scripts/SwingController.cs
+++ Assets/Scripts/SwingController.cs
@@ -29,8 +29,16 @@ public class SwingController : MonoBehaviour
     private Vector3 previousPlayerPosition;
     private Vector3 previousRopePosition;
 
-    [SerializeField] public float swingForce = 10f;
+    [Header("Player/Rope Physics var")]
+    [SerializeField] public float StartswingForce = 10f;
+    [SerializeField] public float NewswingForce;
     [SerializeField] private Rigidbody playerRigidBody;
+    [SerializeField] private Rigidbody ropeRigidBody;
+    private float successSwing = 27.5f; 
+
+    private bool isSpaceBarPressed = false;
+    private float spaceBarHoldDuration = 1f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -46,25 +54,47 @@ public class SwingController : MonoBehaviour
 
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // longer you hold space the more force
+        {
+            isSpaceBarPressed = true;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Vector3 swingDirection = CalculateSwingDirection(playerTransform, ropeTransform);
+            NewswingForce = StartswingForce * (spaceBarHoldDuration * 3); //force * timedur * multiplier
+            playerRigidBody.AddForce(swingDirection * NewswingForce, ForceMode.Acceleration);
+            ropeRigidBody.AddForce(swingDirection * NewswingForce, ForceMode.Acceleration);
+            isSpaceBarPressed = false;
+            spaceBarHoldDuration = 0f;
+        }
+
+        if (isSpaceBarPressed)
+        {
+            spaceBarHoldDuration += Time.deltaTime;
+
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         //Check if the player or rope positions have changed
-        if (playerTransform.position != previousPlayerPosition || ropeTransform.position != previousPlayerPosition)
+        //if (playerTransform.position != previousPlayerPosition || ropeTransform.position != previousPlayerPosition)
         {
             // Player or rope position has changed, update the previous positions and perform necessary actions
-            previousPlayerPosition = playerTransform.position;
-            previousRopePosition = ropeTransform.position;
+           // previousPlayerPosition = playerTransform.position;
+           // previousRopePosition = ropeTransform.position;
 
+            //serves no purpose rn
 
             //if (isSwinging)
             //{
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
-                    Vector3 swingDirection = CalculateSwingDirection(ropeTransform, playerTransform);
-                    playerRigidBody.AddForce(swingDirection * swingForce, ForceMode.Impulse);
-                }
-            //}
+           
+                //}
 
         }
 
