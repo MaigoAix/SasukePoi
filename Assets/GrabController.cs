e3877ebaaba4e2180d6086b8ea248bb3b1da20da
MaigoAix <131898761+MaigoAix@users.noreply.github.com>
2023-06-05 14:23:36 +0100

    ClimbingPhysics0.1
    StatesCreated

--- Assets/GrabController.cs
+++ Assets/GrabController.cs
@@ -7,41 +7,81 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(Rigidbody))]
 
-public class GrabController : XRBaseInteractable
-{
+public class GrabController : MonoBehaviour
+{ 
     //[SerializeField] private InputActionReference grabActionReference;
+    public enum ClimbingState { Idle, Climbing, LetGo}
 
-    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    public ClimbingState currentState;
+    public ClimbingState previousState;
+
+    private void Awake()
     {
-        Debug.Log("Select Enter");
-
-        base.OnSelectEntered(args);
-
-
-
-    }
-
-    protected override void OnSelectExiting(SelectExitEventArgs args)
-    {
-        Debug.Log("Select Exit");
-        base.OnSelectExiting(args);
-
-
+        currentState = ClimbingState.Idle;
+        previousState = ClimbingState.Idle;
     }
 
 
 
+    private void Update()
+    {
+        if (currentState != previousState)
+        {
+            switch (currentState)
+            {
+                case ClimbingState.Idle:
+                    Debug.Log("Idle state");
+                    break;
 
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //      grabActionReference.action.performed += grab ();
-    //}
+                case ClimbingState.Climbing:
+                    Debug.Log("Climbing State");
+                    break;
 
-    // private Action<InputAction.CallbackContext> grab()
-    // {
-    //    throw new NotImplementedException();
-    // }
+                case ClimbingState.LetGo:
+                    Debug.Log("Let Go");
+                    break;
 
+                default:
+                    exitGame();
+                    break;
+            }
+            previousState = currentState;
+        }
 
+        if (currentState == ClimbingState.LetGo)
+        {
+            StartCoroutine(IdleStateConv());
+        }
+    }
+
+    private IEnumerator IdleStateConv()
+    {
+        yield return new WaitForSeconds(2f);
+        currentState = ClimbingState.Idle;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ClimbableWall"))
+        {
+            currentState = ClimbingState.Climbing;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (currentState == ClimbingState.Climbing)
+        {
+            if (!collision.gameObject.CompareTag("ClimbableWall"))
+            {
+                currentState = ClimbingState.LetGo;
+            }
+        }
+    }
+
+    private void exitGame()
+    {
+        Debug.Log("something Wrong has occured");
+        Application.Quit(); //tailor this for platform being used;
+    }
 }
